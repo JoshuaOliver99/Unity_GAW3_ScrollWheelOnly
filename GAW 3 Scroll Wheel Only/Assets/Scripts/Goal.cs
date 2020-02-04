2bb96cc2e6406b2af72eb0d40fbc3bbf45854c44
JoshuaOliver99 <56636418+JoshuaOliver99@users.noreply.github.com>
2020-02-04 17:02:39 +0000

    First level and scaling goal
    first iterations of a first level

---
+++ GAW 3 Scroll Wheel Only/Assets/Scripts/Goal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // FOR COLOUR CHANGE
+    //public SpriteRenderer spriteRenderer;
+
+    public float maxScale = 4;
+    public float minScale = 1;
+
+    float counter = 0f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        gameObject.transform.Rotate(Vector3.right);
+
+        if (counter < 1)
+        {
+            Vector3 currentScale = gameObject.transform.localScale;
+            float scale = Mathf.Max(minScale, currentScale.x * 0.9f);
+            gameObject.transform.localScale = Vector3.one * scale;
+
+        }
+        else if (counter > 1 && counter < 2)
+        {
+            Vector3 currentScale = gameObject.transform.localScale;
+            float scale = Mathf.Min(maxScale, currentScale.x * 1.1f);
+            gameObject.transform.localScale = Vector3.one * scale;
+        }
+        else
+            counter = 0;
+    }
+}
